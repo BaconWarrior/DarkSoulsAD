e1c8bd76cf1118cbfc48cf753d7b636f39806fc1
José María Beristain <dedocebabotas@hotmail.com>
2021-11-21 09:50:54 -0600

    hogueras y reespawn

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+
+    public GameObject bonfire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("hoguera")) {
+            bonfire = other.gameObject;
+            bonfire.GetComponentInChildren<ParticleSystem>().Play();
+        }
+
+        if (other.CompareTag("clouds"))
+        {
+            transform.position = bonfire.transform.position;
+        }
+    }
+}
